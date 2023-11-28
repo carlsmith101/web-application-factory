3954f24de7f2518211862a8b67b6633489ee6490
Carl Smith <carlsmith101@live.co.uk>
2023-11-28 15:13:17 +0000

    add CustomWebApplicationFactory

---
+++ SimpleApiTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace SimpleApiTests
+{
+	public class CustomWebApplicationFactory<Startup> : WebApplicationFactory<Startup> where Startup : class
+	{
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration(builder =>
+            {
+                // add in custom config here
+                builder.Build();
+            });
+        }
+    }
+}
+
